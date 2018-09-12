443df5f1aaa6a80c2c72ec6d512ebe801785336b
Anu Khambete <anukhambete@gmail.com>
2018-09-12 16:18:48 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,13 +23,16 @@ def update
   redirect_to post_path(@post)
 end
 
-private
 
-def post_params(*args)
-  params.require(:post).permit(*args)
-end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+
+	def post_params(*args)
+	  params.require(:post).permit(*args)
+	end
+
 end
