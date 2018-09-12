aff9cb9ba635808cea6a88f9c0ffc2547f97d1b1
Anu Khambete <anukhambete@gmail.com>
2018-09-12 16:04:01 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,14 +13,14 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(params.require(:post).permit(:title, :description))
-  @post.save
-  redirect_to post_path(@post)
+	  @post.save
+	  redirect_to post_path(@post)
 	end
 
 	def update
 		@post = Post.find(params[:id])
-  @post.update(params.require(:post).permit(:title))
-  redirect_to post_path(@post)
+  	@post.update(params.require(:post).permit(:title))
+  	redirect_to post_path(@post)
 	end
 
 	def edit
